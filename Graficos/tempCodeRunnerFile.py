b1852525f08006ca0866fc12ef1dab72e0344107
Newtroniano <luisocara.luis@gmail.com>
2024-09-03 17:55:25 -0300

    coisas

--- Graficos/tempCodeRunnerFile.py
+++ Graficos/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-figure
+for i in range(20):
+#     plt.subplot(rows, 2, i+1)
+#     plt.plot(df['Mes'].astype(str)[i], df['Vendas'][i])
+#     plt.title(f'Gráfico {i + 1}')
+#     plt.xlabel('Eixo X')
+#     plt.ylabel('Eixo Y')
